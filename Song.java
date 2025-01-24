fd4623653d2ccc5785aa72153024823445e7ce7c
Joseph Farnham <jfarnham27@students.polytechnic.org>
2025-01-24 09:59:58 -0800

    Added methods to Song.java

--- Song.java
+++ Song.java
@@ -5,19 +5,24 @@
  */
 public class Song {
     //Fields-- what information do we want each Song to store?
-
-
+    private String name;
+    private String artist;
+    private String time;
+    private boolean liked;
 
 
     /**
-     * Constructor-- what information needs to be given to make a Song?
-     * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
+     * Constructor for objects of class Song
+     * 
+     * 
      */
-
-
-
-
-
+    public Song(String name, String artist, String time)
+    {
+        this.name = name;
+        this.artist = artist;
+        this.time = time;
+        liked = false;
+    }
      /**
       * Methods-- what will you want each Song to do?
       * Consider all the getter methods (getName, getArtist, etc.)
@@ -25,6 +30,24 @@ public class Song {
       * song easily as well!
       * What kind of mutator (setter) methods will you need?
       */
-
-
+    public String getName()
+    {
+        return name;
+    }
+    public String getArtist()
+    {
+        return artist;
+    }
+    public String getTime()
+    {
+        return time;
+    }
+    public void switchLike()
+    {
+        liked = !liked;
+    }
+    public boolean isLiked()
+    {
+        return liked;
+    }
 }
